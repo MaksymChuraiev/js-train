8c7754858781cb7f61ad51889bfa6a33eb139f8e
MaksymChuraiev <mchuraev1988@gmail.com>
2021-10-28 13:41:03 +0300

    add exersices with function

--- js/lesson.js
+++ js/lesson.js
@@ -309,3 +309,119 @@
 //         : `Пользователь ${loginToFind} не найден.`;
 // }
 // console.log(findLogin(logins, 'poly1scute'))
+
+
+// =================================================================
+/*
+ * Напиши функцию findSmallesNumber(numbers) для поиска самого маленького числа в массиве,
+ * при условии что числа уникальные (не повторяются).
+ */
+// const findSmallesNumber = function (numbers) {
+//     let smallestNumber = numbers[0];
+//     for (const number of numbers) {
+//         if (number < smallestNumber) {
+//             smallestNumber = number;
+//         }
+//     }
+//     return smallestNumber;
+// }
+// console.log(findSmallesNumber([3, 8, 12, -2, 15, 84]));
+
+
+// ==============================================================
+/*
+ * Напиши функцию changeCase(string) которая заменяет регистр
+ * каждого символа в строке на противоположный.
+ * Например, если строка «JavaScript», то на выходе должена быть строка «jAVAsCRIPT».
+ */
+// const changeCase = function (string) {
+//     const letters = string.split('');
+//     let invertedString = '';
+//     for (const letter of letters) {
+//         const isInLowerCase = letter === letter.toLowerCase();
+//         invertedString += isInLowerCase
+//             ? letter.toUpperCase()
+//             : letter.toLowerCase();
+//     }
+//     return invertedString;
+// }
+// console.log(changeCase('JavaScript'));
+
+
+// ========================================================
+/*
+ * Напиши функцию slugify(string) которая получает строку и возвращает URL-slug
+ * Строка состоит только из букв и пробелов
+ */
+// const slugify = function (string) {
+//     const normalizedTitle = string.toLowerCase();
+//     const words = normalizedTitle.split(' ');
+//     const slug = words.join('-');
+//     return slug;
+// }
+// console.log(slugify('Top 10 benefits of React framework'));
+
+// const slugify = function (strin) {
+//     return strin.toLowerCase().split(' ').join('-');
+// }
+// console.log(slugify('Top 10 benefits of React framework'));
+
+
+// ============================================================
+/*
+ * Псевдомассив arguments и Array.from и ...
+ */
+// const fn = function () {
+//   console.log(arguments);
+//   const args = Array.from(arguments);
+//   console.log(args);
+// };
+// fn(1, 2, 3);
+// fn(1, 2, 3, 4, 5);
+// fn(1, 2, 3, 4, 5, 6, 7);
+
+// const fn = function (a, b, c, ...args) {
+//   console.log(`${a} ${b} ${c}`);
+//   console.log(args);
+// };
+// fn('hello', 1, 2, 3);
+// fn('aloha', 1, 2, 3, 4, 5);
+// fn('hi', 1, 2, 3, 4, 5, 6, 7);
+
+
+// ===============================================================
+/*
+ * Напиши функцию add для сложения произвольного количества аргументов (чисел)
+ * - Операция ... (rest)
+ */
+// const add = function (...args) {
+//     let total = 0;
+//     for (const arg of args) {
+//         total += arg;        
+//     }
+//     return total;
+// }
+// console.log(add(1, 2, 3, 4, 5, 6, 7));
+
+
+// =============================================================
+/*
+ * Напиши функцию filterNumbers(array [, number1, ...]) которая:
+ * - первым аргументом принимает массив чисел
+ * - после первого аргумента может быть произвольное количество других аргументов которые будут числами.
+ * - Функция должна вернуть новый массив, в котором будут только те аргументы, начиная со второго,
+ *   для которых есть аналог в оригинальном массиве.
+ */
+
+// const filterNumbers = function (array, ...args) {
+//     const uniqueElements = [];
+//     for (const element of array) {
+//         if (args.includes(element)) {
+//             uniqueElements.push(element);
+//         }
+//     }
+//     return uniqueElements;
+// };
+// console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); // [2, 3]
+// console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); // [30, 15]
+// console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64)); // [200]
